3822ae9c2dcc6e8befad41cee786fd203af7fac1
jul-os <julia.osnovskaya@mail.ru>
2026-02-16 02:15:43 +0700

    добавлены файлы candle.* и поправлены ошибки

--- main.cpp
+++ main.cpp
@@ -3,30 +3,31 @@
 #include <iostream>
 
 #include "tests.h"
+#include "candle.h"
 
-//массив всех тестов, который мы заполняем в функции initTests
+// массив всех тестов, который мы заполняем в функции initTests
 static std::vector<std::function<bool()>> tests;
 
-//тест 1
+// тест 1
 bool test1()
 {
-  //пример какого-то теста
-  return 42 == (41 + 1); //passed
+  // пример какого-то теста
+  return 42 == (41 + 1); // passed
 }
 
-//тест 2
+// тест 2
 bool test2()
 {
-  //пример какого-то теста
-  return 42 != (41 + 1); //failed
+  // пример какого-то теста
+  return 42 != (41 + 1); // failed
 }
 
-//тест 3
+// тест 3
 bool test3()
 {
-  Candle candle{ 0.0, 3.0, 3.0, 3.0 };
+  Candle candle{0.0, 3.0, 3.0, 3.0};
 
-  //пример какого-то теста
+  // пример какого-то теста
   return candle.high == 3.0;
 }
 
@@ -35,8 +36,8 @@ void initTests()
   tests.push_back(test1);
   tests.push_back(test2);
   tests.push_back(test3);
-  //tests.push_back(test4);
-  //tests.push_back(test5);
+  // tests.push_back(test4);
+  // tests.push_back(test5);
 }
 
 int launchTests()
@@ -44,7 +45,7 @@ int launchTests()
   int total = 0;
   int passed = 0;
 
-  for (const auto& test : tests)
+  for (const auto &test : tests)
   {
     std::cout << "test #" << (total + 1);
     if (test())
@@ -61,7 +62,7 @@ int launchTests()
 
   std::cout << "\ntests " << passed << "/" << total << " passed!" << std::endl;
 
-  //0 = success
+  // 0 = success
   return total - passed;
 }
 
